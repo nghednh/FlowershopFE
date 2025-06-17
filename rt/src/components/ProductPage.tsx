de06b5cc088a898cea7d98dd92db22bbe2c5cc1b
Nam PHAM <127522074+hhannaap@users.noreply.github.com>
2025-06-17 22:03:23 +0700

    Add RecommendedProducts

--- rt/src/components/ProductPage.tsx
+++ rt/src/components/ProductPage.tsx
@@ -15,7 +15,37 @@ const ProductPage: React.FC = () => {
         <ProductDetails name="Rosy Delight Bouquet" description='A beautiful arrangement of fresh roses in various shades of pink, perfect for any occasion.' category='Flesh Flowers' price={29.99} />
         {/* Placeholder product details, replace with actual product data */}
       </div>
-      {/* <RecommendedProducts /> */}
+      <div className="you-may-also-like">
+        <p>You may also like...</p>
+      </div>
+      <RecommendedProducts products={
+        [
+          {
+            id: 1,
+            name: 'Sunshine Yellow Bouquet',
+            imageUrl: 'https://placehold.co/200x200/FFDDC1/800000?text=Sunshine+Yellow+Bouquet',
+            price: 24.99,
+          },
+          {
+            id: 2,
+            name: 'Elegant White Lily Arrangement',
+            imageUrl: 'https://placehold.co/200x200/FFDDC1/800000?text=Elegant+White+Lily+Arrangement',
+            price: 34.99,
+          },
+          {
+            id: 3,
+            name: 'Vibrant Mixed Flower Basket',
+            imageUrl: 'https://placehold.co/200x200/FFDDC1/800000?text=Vibrant+Mixed+Flower+Basket',
+            price: 39.99,
+          },
+          {
+            id: 4,
+            name: 'Classic Red Rose Bouquet',
+            imageUrl: 'https://placehold.co/200x200/FFDDC1/800000?text=Classic+Red+Rose+Bouquet',
+            price: 49.99,
+          },
+        ]
+      } />
       {/* <Footer /> */}
     </div>
   );
---
+++ rt/src/components/ProductPage/RecommendedProducts.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { int } from "zod/v4";
+
+interface RecommendedProductsProps {
+    products: {
+        id: number;
+        name: string;
+        imageUrl: string;
+        price: number;
+    }[];
+}
+
+const RecommendedProducts: React.FC<RecommendedProductsProps> = ({ products }) => {
+    return (
+        <div className="recommended-products">
+            {products.map((product) => (
+                <div key={product.id} className="recommended-product">
+                    <img src={product.imageUrl} />
+                    <h2 className="re-product-name">{product.name}</h2>
+                    <p className="re-product-price">${product.price.toFixed(2)}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default RecommendedProducts;
